5739786cde8d09ba7b7668e167800da7fd5fb702
arnonax <arnonaxelrod@hotmail.com>
2016-04-20 08:58:47 +0300

    Added Xml comments to ElementsContainer (not finished)

--- TestAutomationEssentials.Selenium/ElementsContainer.cs
+++ TestAutomationEssentials.Selenium/ElementsContainer.cs
@@ -6,10 +6,21 @@ using TestAutomationEssentials.Common;
 
 namespace TestAutomationEssentials.Selenium
 {
+	/// <summary>
+	/// Acts as a base class for all objects that can contain DOM elements
+	/// </summary>
 	public abstract class ElementsContainer
 	{
-	    public const int DefaultWaitTimeout = 30;
+		/// <summary>
+		/// The default timeout that is used to wait for elements to appear or disappear
+		/// </summary>
+		public const int DefaultWaitTimeout = 30;
 
+	    /// <summary>
+	    /// Initializes the instance of <see cref="ElementsContainer"/> with the specified description
+	    /// </summary>
+	    /// <param name="description">A description representing the element in the log</param>
+	    /// <exception cref="ArgumentNullException"><paramref name="description"/> is null</exception>
 	    protected ElementsContainer(string description)
 		{
 			if (description == null)
@@ -18,16 +29,43 @@ namespace TestAutomationEssentials.Selenium
 			Description = description;
 		}
 
+		/// <summary>
+		/// Returns the specified frame which is contained inside the current container
+		/// </summary>
+		/// <param name="frameName">The name of the frame</param>
+		/// <param name="description">A description representing the frame in the log</param>
+		/// <returns>An object representing the specified frame</returns>
+		/// <exception cref="ArgumentNullException">Any of the arguments is null</exception>
+		/// <exception cref="TimeoutException">The frame wasn't found after <see cref="DefaultWaitTimeout"/> seconds</exception>
 		public Frame GetFrame(string frameName, string description)
 		{
+			if (frameName == null)
+				throw new ArgumentNullException("frameName");
+			if (description == null)
+				throw new ArgumentNullException("description");
+
 			var locator = By.XPath(string.Format(".//iframe[@name='{0}' or @id='{0}']", frameName));
 			return GetFrame(locator, description);
 		}
 
 		protected internal abstract void Activate();
 
+		/// <summary>
+		/// Returns the frame that matches the 'by' criteria which is contained inside the current container
+		/// </summary>
+		/// <param name="by">The criteria that is used to locate the frame element</param>
+		/// <param name="description">A description representing the frame in the log</param>
+		/// <returns>An object representing the specified frame</returns>
+		/// <exception cref="ArgumentNullException">Any of the arguments is null</exception>
+		/// <exception cref="TimeoutException">The frame wasn't found after <see cref="DefaultWaitTimeout"/> seconds</exception>
+		/// <remarks>If more than 1 frame matches the specified criteria, the first match is returned;</remarks>
 		public Frame GetFrame(By @by, string description)
 		{
+			if (by == null)
+				throw new ArgumentNullException("by");
+			if(description == null)
+				throw new ArgumentNullException("description");
+
 			var element = WaitForElement(@by, description);
 			return new Frame(element);
 		}
