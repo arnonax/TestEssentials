4ebb3c86a39d72894b09e5d065b8a507ef64f42b
arnonax <arnonaxelrod@hotmail.com>
2017-09-07 14:10:28 +0300

    Revert "Moved test WhenATestFailsAndAlsoACleanupActionFailsThenTheTestFailureIsReported to TestBaseTests as it's not working the same in MsTestV2."
    This reverts commit 109108ee07a689b987951b685bde52df4ed12a19.

--- UnitTests/TestAutomationEssentials.UnitTests/TestBaseTests.cs
+++ UnitTests/TestAutomationEssentials.UnitTests/TestBaseTests.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestAutomationEssentials.UnitTests
 {
@@ -9,38 +8,6 @@ namespace TestAutomationEssentials.UnitTests
         protected override ITestClass CreateTestClass(string dllName, TestContext testContext)
         {
             return new TestClassWrapper(dllName, testContext);
-        }
-
-        // The following test fails in MsTestV2 due to the following issue: https://github.com/Microsoft/testfx/issues/265
-        [TestMethod]
-        public void WhenATestFailsAndAlsoACleanupActionFailsThenTheTestFailureIsReported()
-        {
-            var outputFileName = Path.GetFullPath("Output.txt");
-            File.Delete(outputFileName);
-
-            var testClass = CreateTestClass(
-                GetLinePragma() +
-                @"using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Runtime.CompilerServices;
-using TestAutomationEssentials.MSTest;
-using System;
-using System.IO;
-
-[TestClass]
-public class TestClass1 : TestBase
-{
-	[TestMethod]
-	public void TestMethod1()
-	{
-		AddCleanupAction(() => { throw new Exception(""CleanupAction failure...""); });
-		throw new Exception(""TestMethod failure..."");
-	}
-}
-");
-            var testResults = testClass.Execute();
-            Assert.AreEqual(1, testResults.FailedTests, "Failed");
-
-            StringAssert.Contains(testResults.UnitTestResults[0].ErrorMessage, "TestMethod failure...");
         }
     }
 }
