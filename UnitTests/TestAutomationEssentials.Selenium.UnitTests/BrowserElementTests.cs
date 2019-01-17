cf0075828561d223f3b5f0a7f0bda84501299553
arnonax <arnonaxelrod@hotmail.com>
2019-01-17 09:11:48 +0200

    For some reason the test SubmitOnBrowserElementSubmitsTheForm failed on AppVeyor becuase the URL didn't contian the query string. I changed the test to read the querystring using JavaScript instead.

--- UnitTests/TestAutomationEssentials.Selenium.UnitTests/BrowserElementTests.cs
+++ UnitTests/TestAutomationEssentials.Selenium.UnitTests/BrowserElementTests.cs
@@ -59,24 +59,30 @@ namespace TestAutomationEssentials.Selenium.UnitTests
         {
             const string pageSource = @"
 <html>
-<body>
+<head>
+<script>
+function writeQueryString() {
+	document.getElementById('result').innerHTML = window.location.search;
+}
+</script>
+</head>
+<body onload='writeQueryString()'>
 <form>
 <input name='myInput'/>
 <button action='submit' >Submit</button>
 </form>
+<span id='result'/>
 </body>
 </html>";
 
             using (var browser = OpenBrowserWithPage(pageSource))
             {
-                var webDriver = browser.GetWebDriver();
-                var initialUrl = webDriver.Url;
-
                 IWebElement input = browser.WaitForElement(By.Name("myInput"), "my input");
                 input.SendKeys("dummyValue");
                 input.Submit();
+                var result = browser.WaitForElement(By.Id("result"), "Result");
 
-                Assert.AreEqual($"{initialUrl}?myInput=dummyValue", webDriver.Url);
+                Assert.AreEqual("?myInput=dummyValue", result.Text);
             }
         }
 
