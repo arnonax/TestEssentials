5f739cd3a21cc1d26e97cbb6ca706445a7a6f867
arnonax <arnonaxelrod@hotmail.com>
2019-10-31 15:44:17 +0200

    Build succeeded. This means that test #1 is related to the issue.
    Removing test #2, leaving 1, 3, 4.

--- UnitTests/TestAutomationEssentials.Selenium.UnitTests/BrowserElementTests.cs
+++ UnitTests/TestAutomationEssentials.Selenium.UnitTests/BrowserElementTests.cs
@@ -14,33 +14,13 @@ namespace TestAutomationEssentials.Selenium.UnitTests
     [TestClass]
     public class BrowserElementTests : SeleniumTestBase
     {
-//        [TestMethod]
-//        public void ClearClearsTheTextInATextBox()
-//        {
-//            const string pageSource = @"
-//<html>
-//<body>
-//<input id=""my-input"" value=""some value""/>
-//</body>
-//</html>
-//";
-
-//            using (var browser = OpenBrowserWithPage(pageSource))
-//            {
-//                IWebElement webElement = browser.WaitForElement(By.Id("my-input"), "my input");
-//                Assert.AreEqual("some value", webElement.GetAttribute("value"));
-//                webElement.Clear();
-//                Assert.AreEqual(string.Empty, webElement.GetAttribute("value"));
-//            }
-//        }
-
         [TestMethod]
-        public void SendKeysAppendsText()
+        public void ClearClearsTheTextInATextBox()
         {
             const string pageSource = @"
 <html>
 <body>
-<input id=""my-input"" value=""Hello, ""/>
+<input id=""my-input"" value=""some value""/>
 </body>
 </html>
 ";
@@ -48,11 +28,31 @@ namespace TestAutomationEssentials.Selenium.UnitTests
             using (var browser = OpenBrowserWithPage(pageSource))
             {
                 IWebElement webElement = browser.WaitForElement(By.Id("my-input"), "my input");
-                Assert.AreEqual("Hello, ", webElement.GetAttribute("value"));
-                webElement.SendKeys("world!");
-                Assert.AreEqual("Hello, world!", webElement.GetAttribute("value"));
+                Assert.AreEqual("some value", webElement.GetAttribute("value"));
+                webElement.Clear();
+                Assert.AreEqual(string.Empty, webElement.GetAttribute("value"));
             }
         }
+
+//        [TestMethod]
+//        public void SendKeysAppendsText()
+//        {
+//            const string pageSource = @"
+//<html>
+//<body>
+//<input id=""my-input"" value=""Hello, ""/>
+//</body>
+//</html>
+//";
+
+//            using (var browser = OpenBrowserWithPage(pageSource))
+//            {
+//                IWebElement webElement = browser.WaitForElement(By.Id("my-input"), "my input");
+//                Assert.AreEqual("Hello, ", webElement.GetAttribute("value"));
+//                webElement.SendKeys("world!");
+//                Assert.AreEqual("Hello, world!", webElement.GetAttribute("value"));
+//            }
+//        }
 
         [TestMethod]
         public void SubmitOnBrowserElementSubmitsTheForm()
