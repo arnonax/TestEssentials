4a5ebd4b9fe75ac2064f4f8272d596ab8c1cd2a8
arnonax <arnonaxelrod@hotmail.com>
2019-10-31 15:35:58 +0200

    Build gets stuck as expected.
    Removing first tests (leaving 2, 3 ,4)
    
    Also, leaving only the selenium tests in appveyor.yml.

--- UnitTests/TestAutomationEssentials.Selenium.UnitTests/BrowserElementTests.cs
+++ UnitTests/TestAutomationEssentials.Selenium.UnitTests/BrowserElementTests.cs
@@ -14,25 +14,25 @@ namespace TestAutomationEssentials.Selenium.UnitTests
     [TestClass]
     public class BrowserElementTests : SeleniumTestBase
     {
-        [TestMethod]
-        public void ClearClearsTheTextInATextBox()
-        {
-            const string pageSource = @"
-<html>
-<body>
-<input id=""my-input"" value=""some value""/>
-</body>
-</html>
-";
+//        [TestMethod]
+//        public void ClearClearsTheTextInATextBox()
+//        {
+//            const string pageSource = @"
+//<html>
+//<body>
+//<input id=""my-input"" value=""some value""/>
+//</body>
+//</html>
+//";
 
-            using (var browser = OpenBrowserWithPage(pageSource))
-            {
-                IWebElement webElement = browser.WaitForElement(By.Id("my-input"), "my input");
-                Assert.AreEqual("some value", webElement.GetAttribute("value"));
-                webElement.Clear();
-                Assert.AreEqual(string.Empty, webElement.GetAttribute("value"));
-            }
-        }
+//            using (var browser = OpenBrowserWithPage(pageSource))
+//            {
+//                IWebElement webElement = browser.WaitForElement(By.Id("my-input"), "my input");
+//                Assert.AreEqual("some value", webElement.GetAttribute("value"));
+//                webElement.Clear();
+//                Assert.AreEqual(string.Empty, webElement.GetAttribute("value"));
+//            }
+//        }
 
         [TestMethod]
         public void SendKeysAppendsText()
